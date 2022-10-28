771f968f4fd62c442d5a4210cb008da82b085c32
Michał Krawczykowski <mkrawczykowski@gmail.com>
2022-10-28 17:25:56 +0200

    gatsby-config

---
+++ gatsby-starter-hello-world/gatsby-config.js
@@ -0,0 +1,20 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'WPGraphQL',
+        fieldName: 'wpgraphql',
+        url: 'http://wordpress-demo.test/graphql'
+
+      }
+    }
+  ],
+}
