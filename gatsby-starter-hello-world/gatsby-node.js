02eaaaae0e3ac7a9453cc936e77c31a626ba8f27
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-10-28 22:25:01 +0200

    added createPages to gatsby-node

---
+++ gatsby-starter-hello-world/gatsby-node.js
@@ -0,0 +1,27 @@
+exports.createPages = async ({ actions, graphql }) => {
+  const result = await graphql(`
+    {
+      wpgraphql {
+        pages {
+          nodes {
+            id
+            uri
+            content
+          }
+        }
+      }
+    }
+  `)
+
+  const pages = result.data.wpgraphql.pages.nodes;
+
+  pages.forEach(page => {
+    actions.createPage({
+      path: page.uri,
+      component: require.resolve('./src/templates/page-template.js'),
+      context: {
+        id: page.id,
+      }
+    })
+  })
+}
