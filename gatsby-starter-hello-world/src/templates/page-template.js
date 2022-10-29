8799d23e6fb340d474004072ab398400a110b5d7
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-10-29 10:48:03 +0200

    working pageContext in page-template.js

--- gatsby-starter-hello-world/src/templates/page-template.js
+++ gatsby-starter-hello-world/src/templates/page-template.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Layout from '../components/layout';
 
-const PageTemplate = props => {
+
+// pageContext jest w props. Zamiast pisać props.pageContext, możemy zrobić destructuring propsów i wyciągnąć tylko pageContext
+// zawartość pageContext jest brana z 'context' w createPage w gatsby-node.js.
+// Właściwości obiektu context są dodawane do poniższego pageContext oraz jako zmienna do graphQL
+// https://egghead.io/lessons/gatsby-display-wordpress-page-content-in-gatsby-page-template-components
+const PageTemplate = ({ pageContext }) => {
   return (
     <Layout>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(props, null, 2)}</pre> */}
+      { pageContext.id }
     </Layout>
   )
 }
